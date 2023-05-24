37fc08c063821875fb50d1485400a22c7d26cd36
thyeone <thyeonee@gmail.com>
2023-05-24 19:35:37 +0900

    [CRES-27] feat: 버튼 컴포넌트 작성 #4

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,16 @@
+import tw from 'tailwind-styled-components';
+
 type ButtonProps = {
   text: string;
+  className?: React.ComponentProps<'div'>['className'];
 };
 
-const Button = ({ text }: ButtonProps) => {
-  return;
+const Button = ({ text, ...rest }: ButtonProps) => {
+  return <ButtonContainer {...rest}>{text}</ButtonContainer>;
 };
 
 export default Button;
+
+const ButtonContainer = tw.div`
+flex justify-center items-center whitespace-nowrap rounded-full bg-brand cursor-pointer text-white shadow-button
+`;
