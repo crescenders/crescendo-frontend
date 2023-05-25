f4938a52c623237ca3d1818da62b68c3c8b0d9f1
thyeone <thyeonee@gmail.com>
2023-05-25 15:17:54 +0900

    refactor: ShadowButton, NormalButton 구분

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,15 +2,32 @@ import tw from 'tailwind-styled-components';
 
 type ButtonProps = {
   text: string;
+  onClick?: () => void;
   className?: React.ComponentProps<'div'>['className'];
+  isNormal?: boolean;
 };
 
-const Button = ({ text, ...rest }: ButtonProps) => {
-  return <ButtonContainer {...rest}>{text}</ButtonContainer>;
+const Button = ({ text, onClick, isNormal, ...rest }: ButtonProps) => {
+  return isNormal ? (
+    <NormalButton {...rest} onClick={onClick} text={text}>
+      {text}
+    </NormalButton>
+  ) : (
+    <ShadowButton {...rest} onClick={onClick} text={text}>
+      {text}
+    </ShadowButton>
+  );
 };
 
 export default Button;
 
-const ButtonContainer = tw.div`
-flex justify-center items-center whitespace-nowrap rounded-full bg-brand cursor-pointer text-white shadow-button
+const ShadowButton = tw.button<ButtonProps>`
+flex justify-center items-center whitespace-nowrap rounded-full bg-brand cursor-pointer text-white text-[14px] shadow-button
+`;
+
+const NormalButton = tw.button<ButtonProps>`
+${({ text }) =>
+  (text === '취소' && 'bg-white text-black') ||
+  (text === '삭제' && 'text-white bg-warning')}
+flex justify-center items-center whitespace-nowrap rounded-full cursor-pointer text-[14px] w-[105px] h-[39px] border-[1px] border-text-primary font-bold
 `;
