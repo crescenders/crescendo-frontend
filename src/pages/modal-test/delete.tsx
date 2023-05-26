affaaebc444132fb5139ead6ca827b14d48c369d
Lami <Halam.Lee.Lami@gmail.com>
2023-05-26 16:21:35 +0900

    [CRES-24] test: DeleteModal 컴포넌트 테스트용 페이지 생성 #2

---
+++ src/pages/modal-test/delete.tsx
@@ -0,0 +1,30 @@
+import PageLayout from '@components/common/PageLayout';
+import DeleteModal from '@components/modal/DeleteModal';
+
+import { useState } from 'react';
+
+const Basic = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <PageLayout>
+      <button onClick={() => setIsOpen(true)}>DeleteBtn Modal</button>
+      {isOpen && (
+        <DeleteModal
+          isOpen={isOpen}
+          close={closeModal}
+          handleDelete={() => alert('삭제')}
+          title="과제 삭제"
+          firstText="삭제한 결과는 복구할 수 없어요."
+          secondText="삭제 진행하시겠어요?"
+        />
+      )}
+    </PageLayout>
+  );
+};
+
+export default Basic;
