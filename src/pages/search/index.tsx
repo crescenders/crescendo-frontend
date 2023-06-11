7fa1b313fcf2e41bf25ec6120f34c1e09147764e
thyeone <thyeonee@gmail.com>
2023-06-12 02:26:49 +0900

    [CRES-42] feat: 셀렉트 박스 컴포넌트 작성 및 적용 #32

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,59 @@
+import PageLayout from '@components/common/PageLayout';
+import SelectBox from '@components/common/SelectBox';
+import { useState } from 'react';
+
+export type SelectListType = {
+  id: number;
+  name: SortType;
+};
+
+type SortType = '최신순' | '마감순' | '모집중' | '모집완료';
+
+export type SortStateType = {
+  [key in SortType]: boolean;
+};
+
+export const SortingObject: SortStateType = {
+  최신순: false,
+  마감순: false,
+  모집중: false,
+  모집완료: false,
+};
+
+const LEFT_SELECT_OPTION: SelectListType[] = [
+  { id: 1, name: '최신순' },
+  { id: 2, name: '마감순' },
+];
+
+const RIGHT_SELECT_OPTION: SelectListType[] = [
+  { id: 1, name: '모집중' },
+  { id: 2, name: '모집완료' },
+];
+
+const Search = () => {
+  const [isOpen, setIsOpen] = useState<SortStateType>(SortingObject);
+  const [leftValue, setLeftValue] = useState<string>('최신순');
+  const [rightValue, setRightValue] = useState<string>('모집중');
+  return (
+    <PageLayout>
+      <div className="mt-[150px] flex gap-x-2">
+        <SelectBox
+          options={LEFT_SELECT_OPTION}
+          value={leftValue}
+          setValue={setLeftValue}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+        <SelectBox
+          options={RIGHT_SELECT_OPTION}
+          value={rightValue}
+          setValue={setRightValue}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      </div>
+    </PageLayout>
+  );
+};
+
+export default Search;
