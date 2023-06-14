01e0b83832a9264fad423ac2bc8a85399029b602
thyeone <thyeonee@gmail.com>
2023-06-14 18:31:08 +0900

    [CRES-43] feat: 헤더 로그인 상태에 따라 처리하도록 구현 #34

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoginModal from '@components/modal/LoginModal';
 import NavigateList, {
   NavigateListType,
 } from '@components/common/NavigateList';
 import Link from 'next/link';
+import { useRecoilValue } from 'recoil';
+import { userState } from '@recoil/auth';
 
 const NAVIGATE_LIST: NavigateListType[] = [
   { id: 1, text: '마이페이지', path: '/mypage' },
@@ -14,9 +16,17 @@ const NAVIGATE_LIST: NavigateListType[] = [
 ];
 
 const Header = () => {
-  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { isLogin } = useRecoilValue(userState);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isModal, setIsModal] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window !== undefined) setMounted(true);
+    return;
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <header className="fixed flex h-[70px] w-full max-w-[1024px] items-center justify-between bg-white px-7 shadow-header z-[900]">
@@ -29,44 +39,46 @@ const Header = () => {
           className="h-[24px] w-[124px] cursor-pointer"
         />
       </Link>
-      {isLogin ? (
-        <div className="flex cursor-pointer gap-x-7 relative">
-          <span className="text-16 font-medium">스터디 개설</span>
-          <div className="w-[2px] h-7 bg-[#D9D9D9]" />
+      {mounted ? (
+        isLogin ? (
+          <div className="flex cursor-pointer gap-x-7 relative">
+            <span className="text-16 font-medium">스터디 개설</span>
+            <div className="w-[2px] h-7 bg-[#D9D9D9]" />
+            <span
+              className="text-16 font-bold text-brand mr-5 relative"
+              onMouseEnter={(e: React.MouseEvent<HTMLSpanElement>) => {
+                e.stopPropagation();
+                setIsOpen(true);
+              }}
+            >
+              닉네임 님
+            </span>
+            {isOpen && (
+              <div className="absolute flex items-center flex-col top-[50px] left-2/4">
+                <div className="relative w-4 h-4 bg-white rotate-[135deg] top-2 shadow-sm" />
+                <ul
+                  className="flex flex-col gap-y-[1px] bg-[#D1D1D1] shadow-xl z-10"
+                  onMouseLeave={(e: React.MouseEvent<HTMLSpanElement>) => {
+                    e.stopPropagation();
+                    setIsOpen(false);
+                  }}
+                >
+                  {NAVIGATE_LIST.map(({ id, text, path }) => (
+                    <NavigateList key={id} text={text} path={path} />
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        ) : (
           <span
-            className="text-16 font-bold text-brand mr-5 relative"
-            onMouseEnter={(e: React.MouseEvent<HTMLSpanElement>) => {
-              e.stopPropagation();
-              setIsOpen(true);
-            }}
+            className="cursor-pointer text-16 font-bold text-brand"
+            onClick={() => setIsModal(true)}
           >
-            닉네임 님
+            로그인 / 회원가입
           </span>
-          {isOpen && (
-            <div className="absolute flex items-center flex-col top-[50px] left-2/4">
-              <div className="relative w-4 h-4 bg-white rotate-[135deg] top-2 shadow-sm" />
-              <ul
-                className="flex flex-col gap-y-[1px] bg-[#D1D1D1] shadow-xl z-10"
-                onMouseLeave={(e: React.MouseEvent<HTMLSpanElement>) => {
-                  e.stopPropagation();
-                  setIsOpen(false);
-                }}
-              >
-                {NAVIGATE_LIST.map(({ id, text, path }) => (
-                  <NavigateList key={id} text={text} path={path} />
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      ) : (
-        <span
-          className="cursor-pointer text-16 font-bold text-brand"
-          onClick={() => setIsModal(true)}
-        >
-          로그인 / 회원가입
-        </span>
-      )}
+        )
+      ) : null}
 
       {isModal && (
         <LoginModal isOpen={isModal} handleClose={() => setIsModal(false)} />
--- src/components/common/NavigateList.tsx
+++ src/components/common/NavigateList.tsx
@@ -1,4 +1,7 @@
+import { userState } from '@recoil/auth';
+import { deleteToken } from '@utils/token';
 import Link from 'next/link';
+import { useSetRecoilState } from 'recoil';
 
 export type NavigateListType = {
   id?: number;
@@ -7,9 +10,20 @@ export type NavigateListType = {
 };
 
 const NavigateList = ({ text, path }: NavigateListType) => {
+  const setUserInfo = useSetRecoilState(userState);
+  const handleLogout = () => {
+    if (text === '로그아웃') {
+      deleteToken();
+      setUserInfo({ isLogin: false, username: '' });
+    }
+  };
+
   return (
     <Link href={`${path}`}>
-      <li className="flex h-[47px] items-center justify-center bg-white px-7">
+      <li
+        className="flex h-[47px] items-center justify-center bg-white px-7"
+        onClick={handleLogout}
+      >
         <span className="text-16 font-medium whitespace-nowrap hover:font-bold hover:text-brand">
           {text}
         </span>
