4669064f49cdd48bcd93a2354f4382705df333f0
thyeone <thyeonee@gmail.com>
2023-07-18 16:21:34 +0900

    refactor: Button props를 넘겨 받기 위해 타입 지정 #38

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,19 +1,18 @@
+import { ButtonHTMLAttributes } from 'react';
 import tw from 'tailwind-styled-components';
 
-type ButtonProps = {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   text: string;
-  onClick?: () => void;
-  className?: React.ComponentProps<'div'>['className'];
   isNormal?: boolean;
 };
 
-const Button = ({ text, onClick, isNormal, ...rest }: ButtonProps) => {
+const Button = ({ text, isNormal, ...rest }: ButtonProps) => {
   return isNormal ? (
-    <NormalButton {...rest} onClick={onClick} text={text}>
+    <NormalButton {...rest} text={text}>
       {text}
     </NormalButton>
   ) : (
-    <PurpleButton {...rest} onClick={onClick} text={text}>
+    <PurpleButton {...rest} text={text}>
       {text}
     </PurpleButton>
   );
@@ -22,12 +21,32 @@ const Button = ({ text, onClick, isNormal, ...rest }: ButtonProps) => {
 export default Button;
 
 const PurpleButton = tw.button<ButtonProps>`
-flex justify-center items-center whitespace-nowrap rounded-full bg-brand cursor-pointer text-white text-[14px] shadow-button
+  bg-brand
+  shadow-button
+  flex
+  cursor-pointer
+  items-center
+  justify-center
+  whitespace-nowrap
+  rounded-full
+  text-[14px]
+  text-white
 `;
 
 const NormalButton = tw.button<ButtonProps>`
-${({ text }) =>
-  (text === '취소' && 'bg-white text-black') ||
-  (text === '삭제' && 'text-white bg-warning')}
-flex justify-center items-center whitespace-nowrap rounded-full cursor-pointer text-[14px] w-[105px] h-[39px] border-[1px] border-text-primary font-bold
+  ${({ text }) =>
+    (text === '취소' && 'bg-white text-black') ||
+    (text === '삭제' && 'text-white bg-warning')}
+  border-text-primary
+  flex
+  h-[39px]
+  w-[105px]
+  cursor-pointer
+  items-center
+  justify-center
+  whitespace-nowrap
+  rounded-full
+  border-[1px]
+  text-[14px]
+  font-bold
 `;
