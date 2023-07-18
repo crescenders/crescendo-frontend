c8717b36d3ebd56207be39e82985c36d9332c2ff
thyeone <thyeonee@gmail.com>
2023-07-18 16:19:14 +0900

    refactor: 배포 주소, 배포 및 개발 환경 구분 #38

--- config.ts
+++ config.ts
@@ -1,6 +1,8 @@
 export const CONFIG = {
   LOCAL: process.env.NEXT_PUBLIC_LOCAL,
+  DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
   BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  ENV: process.env.NODE_ENV,
   API_KEY: {
     GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID,
   },
