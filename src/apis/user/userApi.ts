3542e4e954d6d32aa66a466cbd910ad7ac050758
thyeone <thyeonee@gmail.com>
2023-07-18 16:20:21 +0900

    refactor: 사용자 정보 api uri 변경으로 인해 수정 #38

--- src/apis/user/userApi.ts
+++ src/apis/user/userApi.ts
@@ -12,17 +12,17 @@ const userApi = {
     return data.results;
   },
   getSingleUser: async (uuid: string): Promise<User> => {
-    const { data } = await instance.get(`/api/v1/auth/users/${uuid}/`);
+    const { data } = await instance.get(`/api/v1/auth/users/${uuid}/profile`);
     return data;
   },
   putUserNickName: async (uuid: string, nickname: string): Promise<User> => {
-    const { data } = await instance.put(`/api/v1/auth/users/${uuid}/`, {
+    const { data } = await instance.put(`/api/v1/auth/users/${uuid}/profile`, {
       username: nickname,
     });
     return data;
   },
   deleteUser: async (uuid: string): Promise<void> => {
-    return await instance.delete(`/api/v1/auth/users/${uuid}/`);
+    return await instance.delete(`/api/v1/auth/users/${uuid}/profile`);
   },
 };
 
