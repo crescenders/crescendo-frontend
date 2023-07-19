e4f31dc8ec0eedf94b10eb1b0271a4777206b657
thyeone <thyeonee@gmail.com>
2023-07-20 02:06:08 +0900

    refactor: $를 사용한 Transient props로 수정 #38

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -6,21 +6,23 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isNormal?: boolean;
 };
 
+type StyledButtonProps = {
+  $text: string;
+};
+
 const Button = ({ text, isNormal, ...rest }: ButtonProps) => {
   return isNormal ? (
-    <NormalButton {...rest} text={text}>
+    <NormalButton {...rest} $text={text}>
       {text}
     </NormalButton>
   ) : (
-    <PurpleButton {...rest} text={text}>
-      {text}
-    </PurpleButton>
+    <PurpleButton {...rest}>{text}</PurpleButton>
   );
 };
 
 export default Button;
 
-const PurpleButton = tw.button<ButtonProps>`
+const PurpleButton = tw.button`
   bg-brand
   shadow-button
   flex
@@ -33,10 +35,10 @@ const PurpleButton = tw.button<ButtonProps>`
   text-white
 `;
 
-const NormalButton = tw.button<ButtonProps>`
-  ${({ text }) =>
-    (text === '취소' && 'bg-white text-black') ||
-    (text === '삭제' && 'text-white bg-warning')}
+const NormalButton = tw.button<StyledButtonProps>`
+  ${({ $text }) =>
+    ($text === '취소' && 'bg-white text-black') ||
+    ($text === '삭제' && 'text-white bg-warning')}
   border-text-primary
   flex
   h-[39px]
