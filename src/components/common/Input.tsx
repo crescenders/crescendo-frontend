7182577bcbc2729b8a574333d6659c871601ce6e
thyeone <thyeonee@gmail.com>
2023-07-21 01:33:47 +0900

    feat: Input 컴포넌트 작성 #42

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,81 @@
+import { REQUIRED } from '@constants/index';
+import Image from 'next/image';
+import { InputHTMLAttributes, forwardRef } from 'react';
+import tw from 'tailwind-styled-components';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  id: string;
+  variant: 'small' | 'middle' | 'large';
+  required?: boolean;
+  label: string;
+  link?: boolean;
+};
+
+type StyledInputProps = {
+  $variant: string;
+  readonly?: boolean;
+  $link?: boolean;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, required, variant, link, ...rest }, ref) => {
+    return (
+      <Container>
+        <LabelContainer>
+          {required && (
+            <span className="text-status-error mr-1">{REQUIRED}</span>
+          )}
+          <Label htmlFor={id}>{label}</Label>
+        </LabelContainer>
+        <div className="flex relative items-center">
+          {link && (
+            <Image
+              src="/svg/link.svg"
+              width={18}
+              height={18}
+              alt="link"
+              className="absolute left-4"
+            />
+          )}
+          <InputBox ref={ref} $variant={variant} $link={link} {...rest} />
+        </div>
+      </Container>
+    );
+  },
+);
+
+Input.displayName = 'Input';
+
+export default Input;
+
+const Container = tw.div`
+  flex
+  flex-col
+  gap-y-2
+`;
+
+const LabelContainer = tw.div`
+  flex
+`;
+
+const Label = tw.label`
+  text-14
+  text-text-secondary
+  font-bold
+`;
+
+const InputBox = tw.input<StyledInputProps>`
+  ${({ $variant }) =>
+    ($variant === 'small' && 'w-[270px]') ||
+    ($variant === 'middle' && 'w-[340px]') ||
+    ($variant === 'large' && 'w-[550px]')}
+  ${({ readOnly }) => readOnly && 'text-text-primary bg-[#F0F0F0]'}
+  ${({ $link }) => ($link ? 'px-11' : 'pl-[18px]')}
+  border-line-primary
+  focus:border-brand
+  placeholder:text-14
+  h-11
+  rounded-lg
+  border-[1px]
+  outline-none
+`;
