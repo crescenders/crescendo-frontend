5681e7f2de86790ca02d22ebad106cb46bf77f1f
thyeone <thyeonee@gmail.com>
2023-08-07 16:43:33 +0900

    [CRES-55] docs: 커스텀훅 JSDoc 주석 작성 #48

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,6 +1,17 @@
 import { modalComponentState, modalVisibleState } from '@recoil/modal';
 import { useSetRecoilState } from 'recoil';
 
+/**
+ * @description 모달을 글로벌하게 열고 닫을 수 있는 커스텀 훅입니다.
+ * @example
+ * const App = () => {
+ *  const { openModal } = useModal();
+ *
+ *  return (
+ *    <button onClick={openModal(<LoginModal/>)}></button>
+ *  )
+ * }
+ */
 const useModal = () => {
   const setIsShowModal = useSetRecoilState(modalVisibleState);
   const setModal = useSetRecoilState(modalComponentState);
