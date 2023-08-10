caac5fe841762accf8f5b88c9249a25f5bdd40a7
taeyoung <rlaxodud911@gmail.com>
2023-08-10 10:18:12 +0900

    [CRES-44] modify: useCallback 제거 #47

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,26 +1,19 @@
 import { ToastMessageType, toastMessageState } from '@recoil/toast';
-import { useCallback } from 'react';
 import { useSetRecoilState } from 'recoil';
 
 const useToast = () => {
   const setToasts = useSetRecoilState(toastMessageState);
 
-  const hideToast = useCallback(
-    (toastId: number) => {
-      setToasts((current) => current.filter((toast) => toast.id !== toastId));
-    },
-    [setToasts],
-  );
+  const hideToast = (toastId: number) => {
+    setToasts((current) => current.filter((toast) => toast.id !== toastId));
+  };
 
-  const showToast = useCallback(
-    (toast: ToastMessageType) => {
-      const toastId = new Date().getTime();
+  const showToast = (toast: ToastMessageType) => {
+    const toastId = new Date().getTime();
 
-      setToasts((current) => [...current, { ...toast, id: toastId }]);
-      setTimeout(() => hideToast(toastId), 1000);
-    },
-    [setToasts, hideToast],
-  );
+    setToasts((current) => [...current, { ...toast, id: toastId }]);
+    setTimeout(() => hideToast(toastId), 1000);
+  };
 
   return { showToast };
 };
