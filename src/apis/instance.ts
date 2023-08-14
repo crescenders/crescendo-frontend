6e6d8d89a09ca5d8fb989c08525bb5140a2d5f39
thyeone <thyeonee@gmail.com>
2023-08-14 17:46:58 +0900

    [CRES-61] feat: 간단한 토큰 재발급 구현 #59

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,5 +1,5 @@
 import { CONFIG } from '@config';
-import { getToken } from '@utils/token';
+import { deleteToken, getToken, setToken } from '@utils/token';
 import axios from 'axios';
 import authApi from '@apis/auth/authApi';
 
@@ -7,10 +7,12 @@ const instance = axios.create({
   baseURL: CONFIG.BASE_URL,
 });
 
-const ReissuanceToken = async (): Promise<string> => {
+const ReissuanceToken = async (): Promise<string | undefined> => {
   const refreshToken = getToken().refreshToken;
-  const res = await authApi.refreshToken(refreshToken);
-  return res.access;
+  if (refreshToken) {
+    const res = await authApi.refreshToken(refreshToken);
+    return res.access;
+  }
 };
 
 instance.interceptors.request.use(
@@ -20,6 +22,29 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { config: originalRequest, response } = error;
+    if (response?.status === 401) {
+      const accessToken = await ReissuanceToken();
+      if (accessToken) {
+        setToken({ accessToken, refreshToken: getToken().refreshToken });
+        return instance.request(originalRequest);
+      }
+      alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+      deleteToken();
+      CONFIG.ENV === 'development'
+        ? (window.location.href = `${CONFIG.LOCAL}`)
+        : `${CONFIG.DOMAIN}`;
+    }
+
     return Promise.reject(error);
   },
 );
