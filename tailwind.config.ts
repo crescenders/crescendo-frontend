d0750cc4bb9bf30a8e666055ad0eed4bab6bcfa4
taeyoung <rlaxodud911@gmail.com>
2023-08-17 06:18:42 +0900

    [CRES-53] design: slideDown 애니메이션 추가 #56

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,6 +55,19 @@ module.exports = {
         bounce1: 'bounce 0.5s infinite',
         bounce2: 'bounce 0.5s infinite 50ms',
         bounce3: 'bounce 0.5s infinite 100ms',
+        slideDown: 'slide 0.3s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
+      keyframes: {
+        slide: {
+          '0%': {
+            transform: 'translateY(-100px)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: 1,
+          },
+        },
       },
     },
   },
