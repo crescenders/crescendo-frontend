88d79ded2bcdec1ebb98a7ee924fdb432420bad4
taeyoung <rlaxodud911@gmail.com>
2023-08-17 15:03:56 +0900

    [CRES-62] feat: 변경한 닉네임을 userInfo 에도 반영 #63

--- src/hooks/mutations/usePutUser.ts
+++ src/hooks/mutations/usePutUser.ts
@@ -1,12 +1,18 @@
 import userApi from '@apis/user/userApi';
 import useToast from '@hooks/useToast';
 import { useMutation } from 'react-query';
+import { userState } from '@recoil/auth';
+import { useSetRecoilState } from 'recoil';
 
 export const usePutUser = () => {
   const { showToast } = useToast();
+  const setUserInfo = useSetRecoilState(userState);
 
   return useMutation((nickname: string) => userApi.putUser(nickname), {
-    onSuccess: () => {
+    onSuccess: (_, nickname) => {
+      setUserInfo((info) => {
+        return { ...info, username: nickname };
+      });
       showToast({
         type: 'success',
         message: '프로필을 저장했어요.',
