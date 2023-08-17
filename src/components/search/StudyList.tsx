ca58478cd477a8a392b4dd2fe392af9ddab7ae89
thyeone <thyeonee@gmail.com>
2023-08-17 18:11:30 +0900

    [CRES-64] refactor: StudyList 컴포넌트 분리 #61

---
+++ src/components/search/StudyList.tsx
@@ -0,0 +1,66 @@
+import { useGetStudyByKeyword } from '@hooks/queries/useGetStudy';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useIntersection from '@hooks/useIntersection';
+import StudyListSkeleton from '@components/skeleton/StudyListSkeleton';
+import Card from '@components/common/Card';
+
+const StudyList = () => {
+  const router = useRouter();
+  const { targetRef, isIntersecting } = useIntersection({ threshold: 0.4 });
+  const {
+    data: studies,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useGetStudyByKeyword(router.query.keyword as string);
+
+  useEffect(() => {
+    if (isIntersecting && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [isIntersecting]);
+
+  return (
+    <>
+      {studies?.pages.map(({ studies }) =>
+        studies.map(
+          ({
+            id,
+            title,
+            studyName,
+            writer,
+            tags,
+            isCanApply,
+            img,
+            participant,
+            personnel,
+            startDate,
+            endDate,
+          }) => (
+            <Card
+              path="/"
+              key={id}
+              size="big"
+              title={title}
+              studyName={studyName}
+              writer={writer}
+              tags={tags}
+              isCanApply={isCanApply}
+              img={img}
+              participant={participant}
+              personnel={personnel}
+              startDate={startDate}
+              endDate={endDate}
+            />
+          ),
+        ),
+      )}
+      {isFetchingNextPage && <StudyListSkeleton />}
+      <div className="absolute bottom-0" ref={targetRef} />
+    </>
+  );
+};
+
+export default StudyList;
