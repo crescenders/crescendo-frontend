82e9454486295dcd5a5271a52d9dace76a9e89f1
taeyoung <rlaxodud911@gmail.com>
2023-08-18 03:38:10 +0900

    [CRES-62] refactor: 내 정보 조회 쿼리 enabled 삭제 #63

--- src/hooks/queries/useGetProfile.ts
+++ src/hooks/queries/useGetProfile.ts
@@ -1,9 +1,6 @@
 import userApi from '@apis/user/userApi';
-import { getToken } from '@utils/token';
 import { useQuery } from 'react-query';
 
 export const useGetProfile = () => {
-  return useQuery(['useGetProfile'], () => userApi.getUser(), {
-    enabled: !!getToken().accessToken,
-  });
+  return useQuery(['useGetProfile'], () => userApi.getUser());
 };
