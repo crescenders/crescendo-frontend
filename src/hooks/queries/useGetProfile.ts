dbc3f842dc4747a96a9bbe2a4c9e875e862d9229
thyeone <thyeonee@gmail.com>
2023-08-19 16:04:08 +0900

    fix: enabled 옵션 추가

--- src/hooks/queries/useGetProfile.ts
+++ src/hooks/queries/useGetProfile.ts
@@ -1,6 +1,9 @@
 import userApi from '@apis/user/userApi';
+import { getToken } from '@utils/token';
 import { useQuery } from 'react-query';
 
 export const useGetProfile = () => {
-  return useQuery(['useGetProfile'], () => userApi.getUser());
+  return useQuery(['useGetProfile'], () => userApi.getUser(), {
+    enabled: !!getToken().accessToken,
+  });
 };
