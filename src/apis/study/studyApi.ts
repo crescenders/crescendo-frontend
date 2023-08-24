33ce5ea3b604c7dc60616aef4a9f67cf6179b402
thyeone <thyeonee@gmail.com>
2023-08-24 16:28:04 +0900

    [CRES-67] feat: API & 타입 정의 #67

--- src/apis/study/studyApi.ts
+++ src/apis/study/studyApi.ts
@@ -4,10 +4,14 @@ const studyApi = {
   getStudyByKeyword: async (
     keyword: string,
     page: number,
-  ): Promise<Study[]> => {
+  ): Promise<StudyList[]> => {
     const { data } = await instance.get(
       `/api/v1/studies?keyword=${keyword}&page=${page}`,
     );
+    return data;
+  },
+  getStudyDetail: async (id: number): Promise<StudyDetail> => {
+    const { data } = await instance.get(`/api/v1/studygroup/studies/${id}/`);
     return data;
   },
 };
