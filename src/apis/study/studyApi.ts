3a45295d97a4980edc0bb282dec4e394554a3489
taeyoung <rlaxodud911@gmail.com>
2023-08-27 07:50:41 +0900

    [CRES-66] feat: API 정의 #66

--- src/apis/study/studyApi.ts
+++ src/apis/study/studyApi.ts
@@ -10,6 +10,16 @@ const studyApi = {
     );
     return data;
   },
+  createStudy: async (formData: FormData): Promise<Study> => {
+    const { data } = await instance.post(
+      '/api/v1/studygroup/studies',
+      formData,
+      {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      },
+    );
+    return data;
+  },
 };
 
 export default studyApi;
