0e6ecc6214f739cb31f112b06112fc765d010b5d
taeyoung <rlaxodud911@gmail.com>
2023-09-10 02:18:19 +0900

    [CRES-75] feat: 스터디 개설 성공시 개설한 글로 이동하는 동작 추가 #73

--- src/hooks/mutations/useCreateStudy.ts
+++ src/hooks/mutations/useCreateStudy.ts
@@ -1,12 +1,15 @@
 import studyApi from '@apis/study/studyApi';
 import useToast from '@hooks/useToast';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
 
 export const useCreateStudy = () => {
   const { showToast } = useToast();
+  const router = useRouter();
 
   return useMutation((formData: FormData) => studyApi.createStudy(formData), {
-    onSuccess: () => {
+    onSuccess: ({ uuid }) => {
+      router.push(`/study/detail/${uuid}`);
       showToast({
         type: 'success',
         message: '글을 작성했어요.',
