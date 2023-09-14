8adc376d5442b5e33cbaf5cf273637cf784baa3a
thyeone <thyeonee@gmail.com>
2023-09-14 21:35:39 +0900

    refactor: enabled 옵션 제거 #87

--- src/hooks/queries/useGetProfile.ts
+++ src/hooks/queries/useGetProfile.ts
@@ -1,10 +1,8 @@
 import userApi from '@apis/user/userApi';
 import { useQuery } from '@tanstack/react-query';
-import { getToken } from '@utils/token';
 
 export const useGetProfile = () => {
   return useQuery(['useGetProfile'], () => userApi.getUser(), {
-    enabled: !!getToken().accessToken,
     staleTime: Infinity,
     cacheTime: Infinity,
   });
