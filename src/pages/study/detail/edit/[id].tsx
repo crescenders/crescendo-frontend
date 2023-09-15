6b265b0e80014391cdfeb00872dde191cbc83908
taeyoung <rlaxodud911@gmail.com>
2023-09-16 03:06:04 +0900

    [CRES-80] feat: 스터디 수정 페이지 구현 #82

---
+++ src/pages/study/detail/edit/[id].tsx
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/router';
+import PageLayout from '@components/common/PageLayout';
+import Button from '@components/common/Button';
+import Loader from '@components/common/Loader';
+import StudyForm from '@components/form/StudyForm';
+import { useEditStudy } from '@hooks/mutations/useEditStudy';
+
+const EditStudy = () => {
+  const router = useRouter();
+  const id = String(router.query.id);
+  const { mutate: editStudy, isLoading } = useEditStudy();
+
+  return (
+    <PageLayout>
+      {isLoading ? (
+        <Loader isFull />
+      ) : (
+        <div className="mb-[40px] mt-[90px] flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-[34px]">
+            <StudyForm
+              id={id}
+              onSubmit={(formData: FormData) => editStudy({ id, formData })}
+            />
+            <div className="flex gap-3 self-end">
+              <Button
+                isNormal
+                type="button"
+                text="취소"
+                className="h-[40px] w-[60px]"
+                onClick={() => router.replace(`/study/detail/${id}`)}
+              />
+              <Button
+                form="study"
+                text="글 수정"
+                className="h-[40px] w-[80px]"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </PageLayout>
+  );
+};
+
+export default EditStudy;
