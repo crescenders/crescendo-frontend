013244769f0bb5b5f9f33f2ceb837a92960de72b
thyeone <thyeonee@gmail.com>
2023-09-23 18:32:29 +0900

    refactor: 스터디 응답 타입 수정 #93

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -5,26 +5,3 @@ type TResponse = {
 };
 
 type TDate = Date | null;
-
-type TLink = {
-  rel: string;
-  href: string;
-};
-
-type TLeaders = {
-  username: string;
-  _links: TLink[];
-};
-
-type TStudy = {
-  head_image: string;
-  leaders: TLeaders[];
-  post_title: string;
-  study_name: string;
-  until_deadline: number;
-  is_closed: boolean;
-  tags: string[];
-  categories: string[];
-  current_member_count: number;
-  member_limit: number;
-};
