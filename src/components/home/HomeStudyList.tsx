f28fe31bbeb31a0ca10ac97ee2894da522425516
thyeone <thyeonee@gmail.com>
2023-10-02 03:18:11 +0900

    feat: 메인페이지 스터디 랜덤노출 구현 #112

---
+++ src/components/home/HomeStudyList.tsx
@@ -0,0 +1,55 @@
+import Card from '@components/common/Card';
+import { useGetRandomStudyGroupList } from '@hooks/queries/useGetStudy';
+import tw from 'tailwind-styled-components';
+
+const HomeStudyList = () => {
+  const { data } = useGetRandomStudyGroupList();
+
+  return (
+    <StudyListContainer>
+      {data?.results
+        .slice(0, 4)
+        .map(
+          ({
+            uuid,
+            head_image,
+            leaders,
+            post_title,
+            study_name,
+            is_closed,
+            tags,
+            current_member_count,
+            member_limit,
+            until_deadline,
+          }) => (
+            <Card
+              path={`/study/detail/${uuid}`}
+              key={uuid}
+              size="big"
+              title={post_title}
+              studyName={study_name}
+              writer={leaders[0] && leaders[0].username}
+              tags={tags}
+              isClosed={is_closed}
+              img={head_image}
+              participant={current_member_count}
+              personnel={member_limit}
+              deadline={until_deadline}
+            />
+          ),
+        )}
+    </StudyListContainer>
+  );
+};
+
+export default HomeStudyList;
+
+const StudyListContainer = tw.div`
+  flex
+  w-full
+  flex-wrap
+  items-center
+  justify-center
+  gap-8
+  pb-8
+`;
