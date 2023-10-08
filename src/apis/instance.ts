ddfafc010f92afae515f72228d47513e3be562d6
thyeone <thyeonee@gmail.com>
2023-10-09 03:29:47 +0900

    fix: 401 에러 내에서 조건 분기처리 #120

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -25,20 +25,31 @@ instance.interceptors.response.use(
   },
   async (error) => {
     const { config: originalRequest, response } = error;
-    if (response?.status === 401) {
-      const refreshToken = getCookie('refreshToken');
-      try {
-        const accessToken = await authApi
-          .refreshToken(refreshToken)
-          .then((res) => res.access);
-        setToken({ accessToken });
-        // 기존 header에 새로운 accessToken을 설정합니다.
-        instance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-        // 원래 요청에도 새로운 accessToken을 설정합니다.
-        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
 
-        return instance.request(originalRequest);
-      } catch (error) {
+    if (response?.status === 401) {
+      if (
+        response.data.messages &&
+        response.data?.messages[0].token_type === 'access'
+      ) {
+        const refreshToken = getCookie('refreshToken');
+        try {
+          const accessToken = await authApi
+            .refreshToken(refreshToken)
+            .then((res) => res.access);
+          setToken({ accessToken });
+          // 기존 header에 새로운 accessToken을 설정합니다.
+          instance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+          // 원래 요청에도 새로운 accessToken을 설정합니다.
+          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+          return instance.request(originalRequest);
+        } catch (error) {
+          alert('세션이 만료되었습니다. 다시 로그인을 시도해주세요.');
+          process.env.NODE_ENV === 'development'
+            ? (window.location.href = `${process.env.NEXT_PUBLIC_LOCAL}/login`)
+            : (window.location.href = `${process.env.NEXT_PUBLIC_DOMAIN}/login`);
+        }
+      } else {
         alert('세션이 만료되었습니다. 다시 로그인을 시도해주세요.');
         process.env.NODE_ENV === 'development'
           ? (window.location.href = `${process.env.NEXT_PUBLIC_LOCAL}/login`)
