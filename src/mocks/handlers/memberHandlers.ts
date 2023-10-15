142c4f3b703693d0e00f79559bff86526a8fb037
taeyoung <rlaxodud911@gmail.com>
2023-10-16 02:17:49 +0900

    [CRES-101] refactor: 스터디 멤버 조회 api 모킹 base url 제거 #127

--- src/mocks/handlers/memberHandlers.ts
+++ src/mocks/handlers/memberHandlers.ts
@@ -3,10 +3,7 @@ import memberList from '@mocks/data/memberList.json';
 
 export const memberHandlers = [
   // 스터디 멤버 조회
-  rest.get(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/studygroup/studies/:uuid/members`,
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.delay(300), ctx.json(memberList));
-    },
-  ),
+  rest.get(`/api/v1/studygroup/studies/:uuid/members`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.delay(300), ctx.json(memberList));
+  }),
 ];
