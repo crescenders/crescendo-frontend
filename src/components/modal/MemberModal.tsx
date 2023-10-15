10686b1872aa26e6428f5631c8e96854110420ff
taeyoung <rlaxodud911@gmail.com>
2023-10-16 01:48:43 +0900

    [CRES-101] refactor: 멤버 모달 uuid props 삭제 #127

--- src/components/modal/MemberModal.tsx
+++ src/components/modal/MemberModal.tsx
@@ -1,21 +1,21 @@
 import { Suspense } from 'react';
-import MemberList from '@components/manage/MemberList';
+import MemberList from '@components/member/MemberList';
 import Loader from '@components/common/Loader';
 import ModalPortal from '@components/portal/ModalPortal';
+import ApplicationList from '@components/member/ApplicationList';
 
 type MemberModalProps = {
-  uuid: string;
   title: '참여 신청자' | '스터디원 목록';
 };
 
-const MemberModal = ({ uuid, title }: MemberModalProps) => {
+const MemberModal = ({ title }: MemberModalProps) => {
   return (
     <ModalPortal>
       <div className="flex h-[378px] w-[414px] flex-col items-center justify-between gap-8 p-9">
         <span className="font-bold text-brand">{title}</span>
         <div className="no-scrollbar relative flex h-[250px] w-[342px] flex-col items-center gap-6 overflow-y-scroll">
           <Suspense fallback={<Loader />}>
-            <MemberList uuid={uuid} isApproved={title === '스터디원 목록'} />
+            {title === '스터디원 목록' ? <MemberList /> : <ApplicationList />}
           </Suspense>
         </div>
       </div>
