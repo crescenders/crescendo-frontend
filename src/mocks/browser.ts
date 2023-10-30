273f2762a1e37e87cf7860e5a37a2818010d000c
thyeone <thyeonee@gmail.com>
2023-10-30 22:10:38 +0900

    test: 에러 발생을 위한 API 모킹 #155

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,8 +1,10 @@
 import { SetupWorker, setupWorker } from 'msw';
 import { studyHandlers } from '@mocks/handlers/studyHandlers';
 import { memberHandlers } from '@mocks/handlers/memberHandlers';
+import { assignmentHandlers } from '@mocks/handlers/assignmentHandlers';
 
 export const worker: SetupWorker = setupWorker(
   ...studyHandlers,
   ...memberHandlers,
+  ...assignmentHandlers,
 );
