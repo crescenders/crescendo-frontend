1d7a336b57cccca8acca5a374bb825c1c44e9ffd
taeyoung <rlaxodud911@gmail.com>
2023-11-13 18:50:58 +0900

    [CRES-131] refactor: 스터디 멤버 모달에 에러 바운더리 적용 #194

--- src/components/modal/MemberModal.tsx
+++ src/components/modal/MemberModal.tsx
@@ -3,6 +3,8 @@ import MemberList from '@components/member/MemberList';
 import Loader from '@components/common/Loader';
 import ModalPortal from '@components/portal/ModalPortal';
 import ApplicationList from '@components/member/ApplicationList';
+import ErrorBoundary from '@components/errorboundary/ErrorBoundary';
+import ErrorFallback from '@components/errorboundary/ErrorFallback';
 
 type MemberModalProps = {
   title: '참여 신청자' | '스터디원 목록';
@@ -14,9 +16,11 @@ const MemberModal = ({ title }: MemberModalProps) => {
       <div className="flex h-[378px] w-[414px] flex-col items-center justify-between gap-8 p-9">
         <span className="font-bold text-brand">{title}</span>
         <div className="relative flex h-[250px] w-[342px] flex-col items-center gap-6 overflow-y-scroll">
-          <Suspense fallback={<Loader />}>
-            {title === '스터디원 목록' ? <MemberList /> : <ApplicationList />}
-          </Suspense>
+          <ErrorBoundary fallback={ErrorFallback}>
+            <Suspense fallback={<Loader />}>
+              {title === '스터디원 목록' ? <MemberList /> : <ApplicationList />}
+            </Suspense>
+          </ErrorBoundary>
         </div>
       </div>
     </ModalPortal>
