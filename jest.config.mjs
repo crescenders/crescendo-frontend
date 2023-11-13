f5dde6d7b6f8602e2dddaa9f9168b4c87672aa7a
thyeone <thyeonee@gmail.com>
2023-11-13 17:37:15 +0900

    chore: `jest-dom` 타입을 사용할 수 있도록 설정 #190

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,7 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
 
 export default createJestConfig(config);
