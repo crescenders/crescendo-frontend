290ec1af77f79261cf9cf839a0ab74358ac93107
thyeone <thyeonee@gmail.com>
2023-11-15 04:04:53 +0900

    test: E2E 테스트 작성 #207

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,4 +1,4 @@
-describe('LoginTest', () => {
+describe('Login Test', () => {
   it('비로그인 상태로 로그인 필요 페이지 접근시 /login으로 이동', () => {
     cy.visit('/mypage/edit');
     cy.url().should('include', '/login');
@@ -7,5 +7,12 @@ describe('LoginTest', () => {
   it('비로그인 상태로 로그인 필요 페이지 접근시 /login으로 이동', () => {
     cy.visit('/create');
     cy.url().should('include', '/login');
+  });
+
+  it('로그인 상태로 로그인 페이지에 접근할 경우 토큰은 삭제된다.', () => {
+    cy.setCookie('refreshToken', 'refresh');
+    cy.visit('/login');
+    cy.wait(1000);
+    cy.getCookie('refreshToken').should('be.null');
   });
 });
