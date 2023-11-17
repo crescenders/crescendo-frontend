2f025a229a133db202cfe46a1b58b349780bfd99
thyeone <thyeonee@gmail.com>
2023-11-18 03:11:38 +0900

    refactor: 모바일 페이지 제거 #214

--- src/utils/getDevice.ts
+++
@@ -1,9 +0,0 @@
-export const getDevice = () => {
-  const userAgent =
-    typeof navigator !== 'undefined' ? navigator.userAgent : 'server';
-  const isMobile = !!userAgent.match(
-    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i,
-  );
-
-  return isMobile ? 'mobile' : 'desktop';
-};
