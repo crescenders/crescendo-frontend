9e5b25f5658fe36a350e410189be0537535b9484
TaeHyeon Kim <thyeonee@gmail.com>
2023-11-18 05:15:13 +0900

    fix: 빌드 시 컴파일러가 QuillEditor가 JSX로 반환하지 않아 생기는 문제 수정 #220 (#221)

--- src/components/editor/QuillEditor.tsx
+++ src/components/editor/QuillEditor.tsx
@@ -3,17 +3,20 @@ import dynamic from 'next/dynamic';
 import ReactQuill, { ReactQuillProps } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-type ForwardedQuillComponent = ReactQuillProps & {
+type ForwardedQuillComponentProps = ReactQuillProps & {
   forwardedRef: React.Ref<ReactQuill>;
 };
 
 const QuillEditor = dynamic(
   async () => {
     const { default: QuillComponent } = await import('react-quill');
-    const Quill = ({ forwardedRef, ...props }: ForwardedQuillComponent) => (
+    const Quill = ({
+      forwardedRef,
+      ...props
+    }: ForwardedQuillComponentProps) => (
       <QuillComponent ref={forwardedRef} {...props} />
     );
-    return Quill;
+    return Quill as React.ComponentType<ForwardedQuillComponentProps>;
   },
   {
     loading: () => <Loader />,
