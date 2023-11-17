c93d05d4e21774f28c2171a1e0b05f5fa7150b21
thyeone <thyeonee@gmail.com>
2023-11-18 06:07:14 +0900

    fix: 원래 코드로 복구

--- src/components/editor/QuillEditor.tsx
+++ src/components/editor/QuillEditor.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import Loader from '@components/common/Loader';
+import dynamic from 'next/dynamic';
 import ReactQuill, { ReactQuillProps } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
 type ForwardedQuillComponent = ReactQuillProps & {
   forwardedRef: React.Ref<ReactQuill>;
 };
-
-const QuillComponent = React.lazy(() => import('react-quill'));
-
-const QuillEditor = ({ forwardedRef, ...props }: ForwardedQuillComponent) => {
-  return <QuillComponent ref={forwardedRef} {...props} />;
-};
-
+const QuillEditor = dynamic(
+  async () => {
+    const { default: QuillComponent } = await import('react-quill');
+    const Quill = ({ forwardedRef, ...props }: ForwardedQuillComponent) => (
+      <QuillComponent ref={forwardedRef} {...props} />
+    );
+    return Quill;
+  },
+  {
+    loading: () => <Loader />,
+    ssr: false,
+  },
+);
 export default QuillEditor;
