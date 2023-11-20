3d5e32ecf9980bdf6e1b709a42e2a470911bf36d
thyeone <thyeonee@gmail.com>
2023-11-20 17:01:28 +0900

    faet: 네비게이션 리스트에 스터디 개설 포함하도록 추가 #237

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,9 +2,10 @@ export const REQUIRED = '*' as const;
 
 export const NAVIGATE_LIST = [
   { id: 1, text: '마이페이지', path: '/mypage/status' },
-  { id: 2, text: '스터디 관리', path: '/mypage/manage' },
-  { id: 3, text: '정보 수정', path: '/mypage/edit' },
-  { id: 4, text: '로그아웃', path: '/' },
+  { id: 2, text: '스터디 개설', path: '/create' },
+  { id: 3, text: '스터디 관리', path: '/mypage/manage' },
+  { id: 4, text: '정보 수정', path: '/mypage/edit' },
+  { id: 5, text: '로그아웃', path: '/' },
 ] as const;
 
 export const WEEKDAYS = ['M', 'T', 'W', 'T', 'F', 'S', 'S'] as const;
