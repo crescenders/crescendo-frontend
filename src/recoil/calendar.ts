a091c29d560c03277c848d165d07bcb35d5601e0
taeyoung <rlaxodud911@gmail.com>
2023-12-01 13:36:21 +0900

    feat: 캘린더 컴포넌트 관련 recoil 작성 #259

---
+++ src/recoil/calendar.ts
@@ -0,0 +1,43 @@
+import { format } from 'date-fns';
+import { atom, selector } from 'recoil';
+
+type CalendarOpenType = {
+  date: boolean;
+  range: boolean;
+};
+
+type DateStateType = {
+  deadline: TDate;
+  start_date: TDate;
+  end_date: TDate;
+};
+
+export const calendarOpenState = atom<CalendarOpenType>({
+  key: 'calendarOpenState',
+  default: {
+    date: false,
+    range: false,
+  },
+});
+
+export const dateState = atom<DateStateType>({
+  key: 'dateState',
+  default: {
+    deadline: null,
+    start_date: null,
+    end_date: null,
+  },
+});
+
+export const dateSelector = selector({
+  key: 'dateSelector',
+  get: ({ get }) => {
+    const date = get(dateState);
+    let formattedDate = {};
+    Object.keys(date).map(
+      (key) =>
+        (formattedDate[key] = date[key] ? format(date[key], 'yyyy-MM-dd') : ''),
+    );
+    return formattedDate;
+  },
+});
