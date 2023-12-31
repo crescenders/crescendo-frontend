9965ada3ebeea7b56b174a324f5bdaad8bbd12fe
thyeone <thyeonee@gmail.com>
2023-12-31 23:03:17 +0900

    fix: 검색 필터링을 변경할 때 로딩 UI가 보이지 않는 문제 해결 #276

--- src/components/search/StudyList.tsx
+++ src/components/search/StudyList.tsx
@@ -27,49 +27,51 @@ const StudyList = () => {
   return (
     <>
       <ul className="flex flex-wrap justify-center gap-8 px-5 pb-8">
-        {studies.pages.flatMap((page) => page.results).length > 0 ? (
-          studies.pages.flatMap((pages) =>
-            pages.results.map(
-              ({
-                uuid,
-                head_image,
-                leaders,
-                post_title,
-                study_name,
-                is_closed,
-                tags,
-                current_member_count,
-                member_limit,
-                until_deadline,
-              }) => (
-                <Card
-                  path={`/study/detail/${uuid}`}
-                  key={uuid}
-                  size="big"
-                  title={post_title}
-                  studyName={study_name}
-                  writer={leaders[0] && leaders[0].username} // admin 페이지에서 생성한 게시물은 leaders가 없다. 추후 수정 필요
-                  tags={tags}
-                  isClosed={is_closed}
-                  img={head_image}
-                  participant={current_member_count}
-                  personnel={member_limit}
-                  deadline={until_deadline}
-                />
+        {!isFetching ? (
+          studies.pages.flatMap((page) => page.results).length > 0 ? (
+            studies.pages.flatMap((pages) =>
+              pages.results.map(
+                ({
+                  uuid,
+                  head_image,
+                  leaders,
+                  post_title,
+                  study_name,
+                  is_closed,
+                  tags,
+                  current_member_count,
+                  member_limit,
+                  until_deadline,
+                }) => (
+                  <Card
+                    path={`/study/detail/${uuid}`}
+                    key={uuid}
+                    size="big"
+                    title={post_title}
+                    studyName={study_name}
+                    writer={leaders[0] && leaders[0].username} // admin 페이지에서 생성한 게시물은 leaders가 없다. 추후 수정 필요
+                    tags={tags}
+                    isClosed={is_closed}
+                    img={head_image}
+                    participant={current_member_count}
+                    personnel={member_limit}
+                    deadline={until_deadline}
+                  />
+                ),
               ),
-            ),
+            )
+          ) : (
+            <NotFoundStudyList
+              keyword={
+                (router.query.post_title as string) ||
+                (router.query.study_name as string) ||
+                (router.query.tags as string)
+              }
+            />
           )
-        ) : (
-          <NotFoundStudyList
-            keyword={
-              (router.query.post_title as string) ||
-              (router.query.study_name as string) ||
-              (router.query.tags as string)
-            }
-          />
-        )}
+        ) : null}
+        {(isFetching || isFetchingNextPage) && <StudyListSkeleton />}
       </ul>
-      {isFetchingNextPage && <StudyListSkeleton />}
       <div ref={targetRef} />
     </>
   );
