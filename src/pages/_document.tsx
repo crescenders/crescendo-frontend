cc188ead740a2a16610f09bec14ad0a02f480313
thyeone <thyeonee@gmail.com>
2024-02-15 18:49:00 +0900

    refactor: 메타 태그 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="description"
-          content="관심 있는 스터디를 검색하고 활동해 보세요!"
+          content="프로그래밍, 개발과 관련된 스터디를 모집하고 찾을 수 있는 곳, crescendo"
         />
         <meta
           name="google-site-verification"
@@ -15,10 +15,13 @@ export default function Document() {
         <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ko_KR" />
-        <meta property="og:title" content="Crescendo - 소규모 스터디 플랫폼" />
+        <meta
+          property="og:title"
+          content="Crescendo - 개발자를 위한 스터디 플랫폼"
+        />
         <meta
           property="og:description"
-          content="관심 있는 스터디를 검색하고 활동해 보세요!"
+          content="프로그래밍, 개발과 관련된 스터디를 모집하고 찾을 수 있는 곳, crescendo"
         />
         <meta
           property="og:image"
@@ -27,7 +30,10 @@ export default function Document() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="crescendo-study.site" />
         <meta property="twitter:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
-        <meta name="twitter:title" content="Crescendo - 소규모 스터디 플랫폼" />
+        <meta
+          name="twitter:title"
+          content="Crescendo - 개발자를 위한 스터디 플랫폼"
+        />
         <meta
           name="twitter:description"
           content="관심 있는 스터디를 검색하고 활동해 보세요!"
